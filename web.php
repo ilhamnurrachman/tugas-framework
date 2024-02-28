2467acc68dcfa2bd6d0a30fbf897203e92d50d23
22091397071_ilham nur rachman <117990616+ilhamnurrachman@users.noreply.github.com>
2024-02-28 21:42:18 +0700

    Delete web.php

--- web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Models\Konversi;
-use Iluminate\support\facades\route;
-use App\Http\Controllers\KonversiNilaiController;
-
-Route::get('/nilai', [KonversiNilaiController::class, 'index']);
-Route::post('/nilai', [KonversiNilaiController::class, 'hitung']);
